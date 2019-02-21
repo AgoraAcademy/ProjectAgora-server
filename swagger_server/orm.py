568a92b4c0804012223a99427b419f5f50d62c9d
xct24 <xct24@live.com>
2019-02-21 11:37:38 +0800

    init_db中增加sll_args

--- swagger_server/orm.py
+++ swagger_server/orm.py
@@ -64,7 +64,8 @@ class Project_db(Base):
 
 
 def init_db(uri):
-    engine = create_engine(uri, convert_unicode=True)
+    ssl_args = {'ssl': {'ca': 'config/amazon-rds-ca-cert.pem'}}
+    engine = create_engine(uri, convert_unicode=True, connect_args=ssl_args)
     db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
     Base.query = db_session.query_property()
     Base.metadata.create_all(bind=engine)
