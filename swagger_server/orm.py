4c405d111fdebdf9e03d598324ad0ae72a1677ae
xct24 <xct24@live.com>
2019-02-21 11:41:29 +0800

    尝试修复ca路径

--- swagger_server/orm.py
+++ swagger_server/orm.py
@@ -64,7 +64,7 @@ class Project_db(Base):
 
 
 def init_db(uri):
-    ssl_args = {'ssl': {'ca': 'config/amazon-rds-ca-cert.pem'}}
+    ssl_args = {'ssl': {'ca': './config/amazon-rds-ca-cert.pem'}}
     engine = create_engine(uri, convert_unicode=True, connect_args=ssl_args)
     db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
     Base.query = db_session.query_property()
