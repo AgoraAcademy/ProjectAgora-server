0bf98dc7c0d5954d40dd345495f8b9c3cf4ea05f
xct24 <xct24@live.com>
2019-08-15 18:41:28 +0800

    优化Gunicorn日志管理

--- swagger_server/app.py
+++ swagger_server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Dupilicate of __main__.py for heroku to operate
 import connexion
+import logging
 from flask_cors import CORS
 from swagger_server import encoder
 
@@ -11,3 +12,8 @@ CORS(app.app, expose_headers=["Authorization", "refresh_token"])
 app.add_api('swagger.yaml', arguments={'title': 'ProjectAgora'})
 if __name__ == '__main__':
     app.run(port=8080)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
