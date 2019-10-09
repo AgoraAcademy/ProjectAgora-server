9f331ed38d5002fcd0de391714fd88575992aca0
xct24 <xct24@live.com>
2019-10-09 12:24:34 +0800

    create_engine添加pool_recycle尝试解决reset by peers问题

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-
 import connexion
+import logging
 from flask_cors import CORS
 from swagger_server import encoder
 
 
 app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
-CORS(app.app)
+CORS(app.app, expose_headers=["Authorization", "refresh_token", "openid"], supports_credentials=True)
 app.add_api('swagger.yaml', arguments={'title': 'ProjectAgora'})
 if __name__ == '__main__':
     app.run(port=8080)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.app.logger.handlers = gunicorn_logger.handlers
+    app.app.logger.setLevel(gunicorn_logger.level)
